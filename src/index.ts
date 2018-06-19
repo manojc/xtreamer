cd0754efdec1560d66dab5f5adad17be2440ff3d
Manoj Chalode <chalodem@gmail.com>
2018-06-19 23:23:41 +0100

    master - refactoring

--- src/index.ts
+++ src/index.ts
@@ -31,36 +31,40 @@ export class Xstreamer {
     private _stream(url: string, fileId: string): void {
         let bucket: number = 0;
         let buffer: any = get(url)
-            .on('complete', () => {
+            .on('complete', (): void => {
                 //call success callback function, if provided.
                 if (!!this._config.onSuccess && typeof this._config.onSuccess === "function") {
                     this._config.onSuccess(fileId);
                 }
             })
-            .on('data', (chunk: Buffer) => {
+            .on('data', (chunk: Buffer): void => {
                 ++bucket;
                 this.chunks.push(chunk.toString());
                 if (bucket > this._config.chunkSize) {
                     buffer.pause();
-                    bucket= 0;
-                    //keep sending the processed data through the callback function, if provided.
-                    this._storage.addChunks(fileId, this.chunks)
-                        .then((chunkIds: Array<string>) => {
-                            if (!!this._config.onChunkProcesed && typeof this._config.onChunkProcesed === "function") {
-                                this._config.onChunkProcesed(chunkIds.join(', '));
-                            }
-                            this.chunks = [];
-                            buffer.resume();
-                        })
-                        .catch((error: any) => {
-                            //call error callback function, if provided.
-                            if (!!this._config.onError && typeof this._config.onError === "function") {
-                                this._config.onError(error);
-                            }
-                        });
+                    bucket = 0;
+                    this.insertChunks(fileId, buffer);
                 }
             })
-            .on("error", (error: Error) => {
+            .on("error", (error: Error): void => {
+                //call error callback function, if provided.
+                if (!!this._config.onError && typeof this._config.onError === "function") {
+                    this._config.onError(error);
+                }
+            });
+    }
+
+    private insertChunks(fileId: string, buffer: any): void {
+        //keep sending the processed data through the callback function, if provided.
+        this._storage.addChunks(fileId, this.chunks)
+            .then((chunkIds: Array<string>) => {
+                if (!!this._config.onChunkProcesed && typeof this._config.onChunkProcesed === "function") {
+                    this._config.onChunkProcesed(chunkIds.join(', '));
+                }
+                this.chunks = [];
+                buffer.resume();
+            })
+            .catch((error: any) => {
                 //call error callback function, if provided.
                 if (!!this._config.onError && typeof this._config.onError === "function") {
                     this._config.onError(error);
