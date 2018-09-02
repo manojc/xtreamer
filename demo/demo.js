91b6a0e44419263001690afe1b752cfdec653cce
Manoj Chalode <chalodem@gmail.com>
2018-09-02 14:37:59 +0100

    added logic to parse hierarchy.

--- demo/demo.js
+++ demo/demo.js
@@ -3,13 +3,13 @@ const request = require("request");
 const Xtreamer = require("../lib/index");
 
 const options = {
-    node: "VehicleRemarketing",
+    node: "dataset",
     emitXml: true
 }
 
 let count = 0;
-// https://github.com/manojc/xtagger/blob/gh-pages/demo/23mb.xml?raw=true
-Xtreamer(request.get("http://192.168.1.84:8080/boats.xml"), options)
+Xtreamer(request.get("http://192.168.1.84:8080/23mb.xml"), options)
+// Xtreamer(request.get("http://192.168.1.84:8080/boats.xml"), options)
     .on("xmldata", (data) => {
         if (++count % 1000 === 0) {
             console.log(count);
@@ -17,7 +17,7 @@ Xtreamer(request.get("http://192.168.1.84:8080/boats.xml"), options)
     })
     .on("end", (data) => {
         console.log(count);
-        console.log(data);
+        // console.log(data);
     })
     .on("close", () => {
         console.log("stream closed!");
