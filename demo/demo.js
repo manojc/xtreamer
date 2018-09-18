fed06bcfa9af2ef12e463dae52f558791cdeedbb
Manoj Chalode <chalodem@gmail.com>
2018-09-18 12:23:37 +0100

    master - updated demo to show process time for 680 MB XML file.

--- demo/demo.js
+++ demo/demo.js
@@ -2,18 +2,18 @@ const request = require("request");
 const xtreamer = require("../lib/index");
 
 // 23 MB File
-const node = "dataset";
-const url = "https://raw.githubusercontent.com/manojc/xtagger/gh-pages/demo/23mb.xml";
+// const node = "dataset";
+// const url = "https://raw.githubusercontent.com/manojc/xtagger/gh-pages/demo/23mb.xml";
 
 // 680 MB File
-// const node = "ProteinEntry";
-// const url = "http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/pir/psd7003.xml";
+const node = "ProteinEntry";
+const url = "http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/pir/psd7003.xml";
 
 let count = 0;
-
+console.time("xtreamer");
 const xtreamerTransform = xtreamer(node)
-    .on("data", () => ++count % 100 || console.log(count))
-    .on("end", () => console.log(count))
+    .on("data", (xmlNode) => ++count % 1000 || console.log(count))
+    .on("end", () => { console.log(`\n\nNodes processed - ${count}\nProcessing time - `); console.timeEnd("xtreamer"); })
     .on("error", (error) => console.error(error));
 
 request.get(url).pipe(xtreamerTransform);
